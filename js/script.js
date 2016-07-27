4dedd703c5f0f618343812252ac22d9dac0c2526
Logan Sroufe <lsroufe@gmail.com>
2016-07-26 17:36:03 -0700

    updated html and js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+
+  $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
+    showResults(data.Search);
+   });
+});
+
+function showResults(results) {
+
+    $.each(results, function(index, value){
+      console.log(value.Title);
+
+  });
+};
